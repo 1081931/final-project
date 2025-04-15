e669cc6db63b729b6cc514ceb124062bc49edb2d
1081931 <1081931@lammersvilleusd.net>
2025-04-15 13:45:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,59 @@
+# A program creates a window on your screen using Tkinter.
+
 import tkinter as tk
 
-root =  tk.Tk()
-root.geometry("100*400")
+index = 0
 
-what are we doing 
+questions = [
+
+    "what is the capital of england?",
+
+    ]
+
+answers = [
+
+    ["Poland", "London", "Amsterdam", "Paris"],
+
+    ]
+
+correct_answers = [
+
+    ["London"],
+
+    ]
+
+def check_answer(j, index):
+    if j == correct_answers[index]:
+        yes = tk.Label(text = "eh")
+        yes.pack()
+
+
+def refresh(index, answers):
+    
+    question = tk.Label( text = questions[index])
+    question.pack()
+    answer_choices = answers[index]
+
+    for j in answer_choices:
+        potential_answers = tk.Button( text = j, command = lambda answer = j: check_answer(answer, index))
+        potential_answers.pack()
+
+
+
+refresh(index, answers)
+
+
+def my_button(index):
+
+    index+=1
+    refresh(index)
+
+    print("kunsh typed that")
+
+
+
+# Main window
+root = tk.Tk()
+root.wm_geometry("300x200")
 
 root.mainloop()
