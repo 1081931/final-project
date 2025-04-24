084572cd9152c6c7f463f4dbb31b1fe594baaf5b
kunsh-ahuja <1082218@lammersvilleusd.net>
2025-04-23 23:35:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,43 +5,45 @@ import tkinter as tk
 # Main window
 root = tk.Tk()
 
-
 index = 0
 score = 0
 
 score_list = []
 
 questions = [
-
     "What is the capital of England?",
     "What is the tallest mountain?",
     "What is the longest river?",
     "What is the largest ocean?",
     "What is the smallest planet?",
     "What is the only even prime number?",
-    
+    "How many days are in a year?",
+    "Which country shares a border with France?",
+    "What do bees make?",
     ]
 
 answers = [
-
     ["Poland", "London", "Amsterdam", "Paris"],
     ["Everest", "Kinabalu", "Aconcagua", "McKinley"],
     ["Amazon", "Nile", "Colorado", "Dragon"],
     ["Atlantic", "Pacific", "Indian", "Arctic"],
     ["Saturn", "Jupiter", "Mars", "Mercury"],
-    ["9", "44", "0" ,"2"]
-
+    ["9", "44", "0" ,"2"],
+    ["24", "3000", "200" ,"365"],
+    ["England", "Ukraine", "Lesotho" ,"Spain"],
+    ["Milk", "Juice", "Honey" ,"Soda"],
     ]
 
 correct_answers = [
-
     "London",
     "Everest",
     "Nile",
     "Pacific",
     "Mercury",
     "2",
-    
+    "365",
+    "Spain",
+    "Honey"
     ]
 
 
@@ -65,20 +67,20 @@ def check_answer(answer, questions, index, score):
     delete()
 
     if index < len(questions):
-        check(index, questions, answers, score)
+        display(index, questions, answers, score)
     else:
         end_screen(answer, questions, index, score)
 
 
 
 
-def check(index, questions, answers, score):
+def display(index, questions, answers, score):
 
     question = tk.Label(text = questions[index])
     question.pack()
     answer_choices = answers[index]
 
-    score_display = tk.Label(text = score)
+    score_display = tk.Label(text = "Score: "+str(score))
     score_display.pack()
 
     for j in answer_choices:
@@ -86,27 +88,29 @@ def check(index, questions, answers, score):
         potential_answers.pack()
 
 
-check(index, questions, answers, score)
+display(index, questions, answers, score)
 
 
 
 def end_screen(answer, questions, index, score):
 
     if score < len(questions):
-        message = tk.Label(text = "lmao bozo")
+        message = tk.Label(text = "Game over. Would you like to play again?")
     else:
-        message = tk.Label(text = "gud job")
+        message = tk.Label(text = "Game over. You got all of them right!")
 
     message.pack()
 
-    final_score = tk.Label(text = (str(score) + '/6'))
+    final_score = tk.Label(text = str(score) + '/'+str(len(questions)))
     final_score.pack()
 
     score_list.append(score)
+    high_score = tk.Label(text="Highest score: "+str(max(score_list))+'/'+str(len(questions)))
+    high_score.pack()
     index = 0
     score = 0
 
-    restart = tk.Button( text = "Restart?", command = lambda: check(answer, questions, index, score))
+    restart = tk.Button( text = "Restart?", command = lambda: display(answer, questions, index, score))
 
 
 
